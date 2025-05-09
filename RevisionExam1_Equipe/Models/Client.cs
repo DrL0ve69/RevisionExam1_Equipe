dc29e274202b0d0af1232cba7364ffabeed8a96d
Philippe Nadeau <pscrap00@gmail.com>
2025-05-09 11:58:55 -0400

    Model Client

---
+++ RevisionExam1_Equipe/Models/Client.cs
@@ -0,0 +1,9 @@
+﻿namespace RevisionExam1_Equipe.Models
+{
+    public class Client
+    {
+        public int NumClient { get; set; }
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+    }
+}
