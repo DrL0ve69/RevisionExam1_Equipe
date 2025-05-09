0cc79659ab986e5218a9383cd80bf1b82fef0486
Philippe Nadeau <pscrap00@gmail.com>
2025-05-09 12:15:27 -0400

    Liste Clients

--- RevisionExam1_Equipe/Models/DB_Clients_ViewModel.cs
+++ RevisionExam1_Equipe/Models/DB_Clients_ViewModel.cs
@@ -3,12 +3,12 @@
     public class DB_Clients_ViewModel
     {
         public static List<Client> listeClients = new List<Client>()
-    {
-        new Client(1, "Dupont", "Jean"),
-        new Client(2, "Martin", "Marie"),
-        new Client(3, "Dubé", "François"),
-        new Client(4, "Trudeau", "Mélanie"),
-        new Client(5, "Lambert", "Pierre")
-    };
+        {
+            new Client(1, "Dupont", "Jean"),
+            new Client(2, "Martin", "Marie"),
+            new Client(3, "Dubé", "François"),
+            new Client(4, "Trudeau", "Mélanie"),
+            new Client(5, "Lambert", "Pierre")
+        };
     }
 }
