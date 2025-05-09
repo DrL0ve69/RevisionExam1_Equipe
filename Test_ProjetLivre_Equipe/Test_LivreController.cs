4fe1a5af6e7e3d7abd6e495c997af8787908c23b
phili <0758510@cegepoutaouais.qc.ca>
2025-05-09 15:25:08 -0400

    Test pour les types de retour foncrionnent!

--- Test_ProjetLivre_Equipe/Test_LivreController.cs
+++ Test_ProjetLivre_Equipe/Test_LivreController.cs
@@ -1,11 +1,39 @@
-﻿namespace Test_ProjetLivre_Equipe
+﻿using Microsoft.AspNetCore.Mvc;
+using RevisionExam1_Equipe.Controllers;
+
+namespace Test_ProjetLivre_Equipe
 {
     public class Test_LivreController
     {
         [Fact]
-        public void Test1()
+        public void Test_TypeRetour_Catalogue()
         {
-
+            // Arrange
+            var controller = new LivreController();
+            // Act
+            var result = controller.Catalogue();
+            // Assert
+            Assert.IsType<ViewResult>(result);
+        }
+        [Fact]
+        public void Test_TypeRetour_TrouverLivre()
+        {
+            // Arrange
+            var controller = new LivreController();
+            // Act
+            var result = controller.TrouverLivre(1);
+            // Assert
+            Assert.IsType<ViewResult>(result);
+        }
+        [Fact]
+        public void Test_TypeRetour_Introuvable()
+        {
+            // Arrange
+            var controller = new LivreController();
+            // Act
+            var result = controller.TrouverLivre(111);
+            // Assert
+            Assert.IsType<ContentResult>(result);
         }
     }
 }
