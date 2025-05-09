94dda1b8f6487888400ddb4e49e2df934698e948
DrL0ve69 <146204861+DrL0ve69@users.noreply.github.com>
2025-05-09 11:22:05 -0400

    Ajout classe livre

---
+++ RevisionExam1_Equipe/Models/LivreModel.cs
@@ -0,0 +1,15 @@
+﻿namespace RevisionExam1_Equipe.Models;
+
+public class LivreModel
+{
+    public int Id { get; set; }
+    public string Titre { get; set; }
+    public double Prix { get; set; }
+
+    public LivreModel(int id, string titre, double prix)
+    {
+        Id = id;
+        Titre = titre;
+        Prix = prix;
+    }
+}
