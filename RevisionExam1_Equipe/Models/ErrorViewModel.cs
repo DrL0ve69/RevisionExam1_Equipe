f3deacba4969298e843a9e4d777b69cfc1006f85
DrL0ve69 <146204861+DrL0ve69@users.noreply.github.com>
2025-05-09 11:16:57 -0400

    Ajoutez des fichiers projet.

---
+++ RevisionExam1_Equipe/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RevisionExam1_Equipe.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
