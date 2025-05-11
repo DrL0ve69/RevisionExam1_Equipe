815ed99743f39226ee53a3860a5d3133f4d0ca0a
phili <0758510@cegepoutaouais.qc.ca>
2025-05-10 21:04:05 -0400

    Ajout du LivreModel de base, ID=0, pour afficher non-dispo dans le panier

--- RevisionExam1_Equipe/Models/LivreModel.cs
+++ RevisionExam1_Equipe/Models/LivreModel.cs
@@ -5,7 +5,12 @@ public class LivreModel
     public int Id { get; set; }
     public string Titre { get; set; }
     public double Prix { get; set; }
-
+    public LivreModel() 
+    {
+        Id = 0;
+        Titre = "Non-disponible (Voir ID?)";
+        Prix = 0.0;
+    }
     public LivreModel(int id, string titre, double prix)
     {
         Id = id;
